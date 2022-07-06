f0cd5810d78ec315ffb2f81e00db152b88afe94f
AlexandrRust <123_00@ukr.net>
2022-07-06 12:21:21 +0300

    finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,95 @@
-import { nanoid } from 'nanoid'
+import { nanoid } from 'nanoid';
 
-import { Component } from "react";
-import { PhoneBookForm } from "components/PhoneBookForm/PhoneBookForm";
-import { Section } from "components/Section/Section.styled";
+import { Component } from 'react';
+import { PhoneBookForm } from 'components/PhoneBookForm/PhoneBookForm';
+import { Section } from 'components/Section/Section.styled';
 import { Box } from 'components/Box/Box.styled';
-import { FhoneBookTitle } from "components/Title/Title";
-import { FhonesList } from "components/PhonesList/PhonesList";
+import { FhoneBookTitle } from 'components/Title/Title';
+import { FhonesList } from 'components/PhonesList/PhonesList';
 import { Filter } from 'components/Filter/Filter';
 
-
-
 export class App extends Component {
-  
   state = {
     contacts: [
-      {id: nanoid(), name: 'Rosie Simpson', number: '459-12-56'},
-      {id: nanoid(), name: 'Hermione Kline', number: '443-89-12'},
-      {id: nanoid(), name: 'Eden Clements', number: '645-17-79'},
-      {id: nanoid(), name: 'Annie Copeland', number: '227-91-26'},
+      { id: nanoid(), name: 'Rosie Simpson', number: '459-12-56' },
+      { id: nanoid(), name: 'Hermione Kline', number: '443-89-12' },
+      { id: nanoid(), name: 'Eden Clements', number: '645-17-79' },
+      { id: nanoid(), name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
-  
 
-  formSubmitHandler = ({ name, number}) => {
-    
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const contactsParse = JSON.parse(contacts);
+
+    if (contactsParse) {
+      this.setState({
+        contacts: contactsParse,
+      });
+    }
+  }
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  formSubmitHandler = ({ name, number }) => {
     const contact = {
       id: nanoid(),
       name,
       number,
-    }
-    
-    
+    };
 
-    this.state.contacts.find(elem => elem.name.toLowerCase() === name.toLowerCase()) ? alert(`${name} is already in contacts.`) :
-    this.setState(({ contacts }) => ({
-      contacts: [...contacts, contact],
-    }));
-  }
+    this.state.contacts.find(
+      elem => elem.name.toLowerCase() === name.toLowerCase()
+    )
+      ? alert(`${name} is already in contacts.`)
+      : this.setState(({ contacts }) => ({
+          contacts: [...contacts, contact],
+        }));
+  };
 
-  deleteContact = (id) => {
+  deleteContact = id => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(elem => elem.id !== id),
     }));
-  }
+  };
 
   changeFilter = e => {
-    this.setState({filter: e.currentTarget.value})
-  }
-
+    this.setState({ filter: e.currentTarget.value });
+  };
 
   getVisibleContacts = () => {
     const { filter, contacts } = this.state;
 
     const normalizeFilter = filter.toLocaleLowerCase();
 
-    return contacts.filter(contact => contact.name.toLocaleLowerCase().includes(normalizeFilter))
-  }
-  
+    return contacts.filter(contact =>
+      contact.name.toLocaleLowerCase().includes(normalizeFilter)
+    );
+  };
+
   render() {
     const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts()
-    
+    const visibleContacts = this.getVisibleContacts();
+
     return (
       <Section>
-        <FhoneBookTitle text={"Phonebook"}></FhoneBookTitle>
+        <FhoneBookTitle text={'Phonebook'}></FhoneBookTitle>
         <Box>
           <PhoneBookForm onSubmit={this.formSubmitHandler} />
         </Box>
-        <FhoneBookTitle text={"Contacts"}></FhoneBookTitle>
+        <FhoneBookTitle text={'Contacts'}></FhoneBookTitle>
         <Box>
           <Filter value={filter} onChange={this.changeFilter} />
-          <FhonesList options={visibleContacts} deleteContact={this.deleteContact} />
+          <FhonesList
+            options={visibleContacts}
+            deleteContact={this.deleteContact}
+          />
         </Box>
       </Section>
-    )
-    };
-};
+    );
+  }
+}
